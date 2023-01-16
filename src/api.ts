969187b27d16830d1bf66b5e3102535cd0725f24
Nikhil Kumar <nkhlneil@gmail.com>
2023-01-16 09:26:21 +0100

    Removed wait from API

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,7 @@
 export const BASE_URL = 'https://swapi.dev/api/';
 const FILMS_RESOURCE = 'films/';
 
-function wait(delay: number) {
-  return new Promise(resolve => setTimeout(resolve, delay));
-}
-
-export async function getFilm<T>(): Promise<T> {
-  const url = BASE_URL + FILMS_RESOURCE;
-
-  console.log(url);
-
-  // keeping this delay for testing purpose
-  return wait(500)
-    .then(() => fetch(url))
+export async function get<T>(url: string = BASE_URL + FILMS_RESOURCE): Promise<T> {
+  return fetch(url)
     .then(response => response.json());
 }
